e92f2323ac70e1eb77e64ef20206d2c6127c3b6b
Artyom Khogoev <artikhog@yandex/ru>
2023-05-03 12:37:03 +0300

    add resize_page

---
+++ js/style.js
@@ -0,0 +1,10 @@
+function resize_page() {
+    const height = window.innerHeight;
+    const width = window.innerWidth;
+    const info_width = width - height;
+    document.getElementById('map_div').style.width = height+"px"
+    document.getElementById('map_div').style.height = height+"px"
+    document.getElementById('information_div').style.width = info_width+"px"
+        document.getElementById('information_div').style.height = height+"px"
+    console.log(document.getElementById('map_div').style.width, document.getElementById('map_div').style.height);
+}
